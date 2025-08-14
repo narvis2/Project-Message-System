71044375f4cf95667c851454e778b094d3da3133
narvis2 <choeyeongjun@choeyeongjun-ui-MacBookPro.local>
2025-08-14 17:10:24 +0900

    SessionService 간략화

--- src/main/kotlin/com/narvi/messagesystem/service/SessionService.kt
+++ src/main/kotlin/com/narvi/messagesystem/service/SessionService.kt
@@ -5,7 +5,6 @@ import com.narvi.messagesystem.constant.KeyPrefix
 import com.narvi.messagesystem.dto.domain.UserId
 import com.narvi.messagesystem.kafka.ListenTopicCreator
 import mu.KotlinLogging
-import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.session.Session
 import org.springframework.session.SessionRepository
 import org.springframework.stereotype.Service
@@ -14,7 +13,6 @@ import java.time.Instant
 @Service
 class SessionService(
     private val httpSessionRepository: SessionRepository<out Session>,
-    private val stringRedisTemplate: StringRedisTemplate,
     private val listenTopicCreator: ListenTopicCreator,
     private val cacheService: CacheService,
 ) {
@@ -30,17 +28,7 @@ class SessionService(
         }
     }
 
-    fun removeActiveChannel(userId: UserId): Boolean {
-        val channelIdKey = buildChannelIdKey(userId)
-
-        return try {
-            stringRedisTemplate.delete(channelIdKey)
-            true
-        } catch (ex: Exception) {
-            log.error("Redis Delete failed. key: {},", channelIdKey)
-            false
-        }
-    }
+    fun removeActiveChannel(userId: UserId): Boolean = cacheService.delete(buildChannelIdKey(userId))
 
     fun refreshTTL(userId: UserId, httpSessionId: String) {
         try {
@@ -56,10 +44,12 @@ class SessionService(
     }
 
     private fun buildChannelIdKey(userId: UserId): String =
-        "%s:%d:%s".format(KeyPrefix.USER, userId.id, IdKey.CHANNEL_ID.value)
+        cacheService.buildKey(
+            KeyPrefix.USER, userId.id.toString(), IdKey.CHANNEL_ID.value
+        )
 
     private fun buildUserLocationKey(userId: UserId): String =
-        "%s:%d:%s".format(KeyPrefix.USER_SESSION, userId.id.toString())
+        cacheService.buildKey(KeyPrefix.USER_SESSION, userId.id.toString())
 
     companion object {
         private val log = KotlinLogging.logger {}
